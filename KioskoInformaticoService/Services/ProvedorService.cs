f2114f6aea390952e6bca1d7c2cd2520a41a60a3
LuuxDD <luisinabender@hotmail.com>
2024-10-01 17:30:12 -0300

    Carpeta Report Carpeta DataSet

---
+++ KioskoInformaticoService/Services/ProvedorService.cs
@@ -0,0 +1,31 @@
+﻿using KioskoInformaticoServices.Models;
+using System;
+using KioskoInformaticoServices.Interfaces;
+using KioskoInformaticoServices.Models;
+using KioskoInformaticoServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace KioskoInformaticoServices.Services
+{
+    public class ProvedorService : GenericService<Proveedor>, IProveedorService
+    {
+        public async Task<List<Proveedor>?> GetAllAsync(string? filtro)
+        {
+            var response = await client.GetAsync($"{_endpoint}?filtro={filtro}");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content?.ToString());
+            }
+            return JsonSerializer.Deserialize<List<Proveedor>>(content, options); ;
+        }
+    }
+}
+
+
