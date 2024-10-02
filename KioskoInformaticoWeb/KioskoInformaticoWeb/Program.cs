d795cbb31d317be8e3fe87690404c007c46a8a18
LuuxDD <luisinabender@hotmail.com>
2024-10-02 17:37:48 -0300

    boton eliminar

--- KioskoInformaticoWeb/KioskoInformaticoWeb/Program.cs
+++ KioskoInformaticoWeb/KioskoInformaticoWeb/Program.cs
@@ -1,8 +1,10 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using KioskoInformaticoServices.Interfaces;
 using KioskoInformaticoServices.Services;
 using KioskoInformaticoWeb;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericService<>));
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
