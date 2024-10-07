652f4fe81d2dc9b885f9c93a0d97a4b088f9c91b
LuuxDD <luisinabender@hotmail.com>
2024-10-07 18:36:58 -0300

    Añadidas nuevas vistas y lógica para productos y clientes
    Se han realizado los siguientes cambios:
    - Añadidas referencias de compilación para `Productos.xaml` y `Productos.xaml.cs` en `KioscoInformaticoApp.csproj`.
    - Configuración para excluir `Cliente.razor` de la publicación de un solo archivo en `KioskoInformaticoWeb.csproj`.
    - Eliminada carpeta vacía `Pages\Clientes\` en `KioskoInformaticoWeb.csproj`.
    - Inyección de `SweetAlertService` y método `Eliminar` en `Productos.razor`.
    - Creada clase `ProductosViewModel` en `ProductosViewModel.cs` para la lógica de la vista de productos.
    - Añadidos archivos `Productos.xaml` y `Productos.xaml.cs` para la interfaz y lógica de la vista de productos.
    - Añadida página `Cliente.razor` para gestionar clientes.
    - Añadida página `EditarLocalidad.razor` para editar localidades.

---
+++ KioskoInformaticoApp/KioscoInformaticoApp/ViewModels/ProductosViewModel.cs
@@ -0,0 +1,78 @@
+﻿using KioscoInformaticoApp.Utils;
+using KioskoInformaticoServices.Models;
+using KioskoInformaticoServices.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioscoInformaticoApp.ViewModels
+{
+    public class ProductosViewModel : ObjectNotification
+    {
+        private GenericService<Producto> productoService= new GenericService<Producto>();
+        private string filterProducts;
+
+        public string FilterProducts
+        {
+            get { return filterProducts; }
+            set { filterProducts = value; OnPropertyChanged();
+                                         _ = FiltrarProductos();     
+            }
+        }
+        private bool _isRefreshing;
+        public bool IsRefreshing
+        {
+            get => _isRefreshing;
+
+            set
+            {
+                _isRefreshing = value;
+                OnPropertyChanged();
+            }
+
+        private ObservableCollection<Producto> productos;
+
+        public ObservableCollection<Producto> Productos
+
+        {
+            get { return productos; }
+            set { productos = value; OnPropertyChanged(); }
+        }
+
+        public Command ObtenerProductosCommand { get; set; }
+        public Command FiltrarProductosCommand { get; set; }
+
+        private List<Producto>? productosListToFilter;
+
+        private bool activityStart;
+        public bool ActivityStart
+        {
+            get { return activityStart; }
+            set { activityStart = value; OnPropertyChanged(); }
+        }
+
+        public ProductosViewModel()
+        {
+            ObtenerProductosCommand = new Command(async () => await ObtenerProductos());
+            FiltrarProductosCommand = new Command(async () => await FilterProductsList());
+        }
+
+        private async Task FilterProductsList()
+        {
+            var productosFiltrados = productosListToFilter.Where(p => p.Nombre.Contains(FilterProducts));
+            Productos = new ObservableCollection<Producto>(productosFiltrados); 
+        }
+
+        private async Task ObtenerProductos()
+        {
+            ActivityStart = true;
+            productosListToFilter = await productoService.GetAllAsync();
+            Productos = new ObservableCollection<Producto>(productosListToFilter);
+            ActivityStart = false;
+
+        }
+    }
+}
